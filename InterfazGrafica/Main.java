94fe1ba85d38af935af42737ae186c927b370dd7
HazelCasas <carloscasas61@aragon.unam.mx>
2023-05-01 01:06:33 -0600

    Interface gráficas.

---
+++ InterfazGrafica/Main.java
@@ -0,0 +1,11 @@
+package TAREAS.InterfazGrafica;
+
+import TAREAS.InterfazGrafica.IU.Principal;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Principal ventana = new Principal("Hola Mundo AWT");
+    }
+}
